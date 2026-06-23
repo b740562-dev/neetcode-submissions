b9a925c7697c112699295f04d592d9a30c7caf8a
Bebo Modi <b740562@gmail.com>
2026-06-23 23:09:23 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t="";
+        for(char c:s){
+            if(isalnum(c)){
+                t+=tolower(c);
+            }
+        }
+
+        int n=t.size();
+        for(int i=0;i<n/2;i++){
+            if(t[i]!=t[n-1-i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
