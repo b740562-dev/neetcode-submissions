e20c9ae0f88222c8cb9a54aabd9b738d5a9a3472
Bebo Modi <b740562@gmail.com>
2026-06-24 16:37:51 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,62 @@
+class Solution {
+private:
+    vector<int> suffixmax(vector<int>& arr){
+        int n=arr.size();
+        vector<int> ans(n,0);
+        int suffix=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>suffix){
+                ans[i]=arr[i];
+                suffix=arr[i];
+            }
+            else{
+                ans[i]=suffix;
+            }
+        }
+        return ans;
+    }
+public:
+    int trap(vector<int>& arr) {
+        // see for each index we can calculate how much water can be trapped on top of it !!!
+        // at each index water we can trap is min(leftmax,rightmax)-arr[index]!!!
+        // summing for all the indexes gives the final ans !!!!!
+        // int n=arr.size(),total=0;
+        // vector<int> rightmax=suffixmax(arr);
+        // int leftmax=arr[0];
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]>leftmax){
+        //         leftmax=arr[i];
+        //     }
+        //     total+=min(leftmax,rightmax[i])-arr[i];
+        // }
+        // return total;
+
+        // ---------------------------------OPTIMAL-------------------------------------------------------
+
+        // optimal approach is to use sliding window !!!
+
+        int l=0, r=arr.size()-1,total=0;
+        int leftmax=0, rightmax=0;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(arr[l]<leftmax){
+                    total+=leftmax-arr[l];
+                }
+                else{
+                    leftmax=arr[l];
+                }
+                l++;
+            }
+            else{
+                if(arr[r]<rightmax){
+                    total+=rightmax-arr[r];
+                }
+                else{
+                    rightmax=arr[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+};
