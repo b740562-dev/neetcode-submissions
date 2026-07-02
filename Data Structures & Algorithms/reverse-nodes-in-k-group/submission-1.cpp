e8cf911a7d111174cb7f0849a8acb2c1c074e4bb
Bebo Modi <b740562@gmail.com>
2026-07-02 23:49:59 +0530

    Add: reverse-nodes-in-k-group - submission-1

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-1.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+private:
+    ListNode* reverse(ListNode* head){
+
+        ListNode* temp=head;
+        ListNode* prev=nullptr;
+
+        while(temp!=nullptr){
+            ListNode* nex=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=nex;
+        }
+        return prev;
+
+    }
+
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* temp=head;
+
+        // add prevtail node to make sure each k nodes group remain conencted !!!
+        ListNode* prevtail=dummy;
+
+        while(temp!=nullptr){
+
+            // currently kon sa node head hai !!!
+            ListNode* currhead=temp;
+
+            // we want to stop at kth node !!!!!
+            for(int i=0;i<k-1;i++){
+                if(temp!=nullptr){
+                    temp=temp->next;
+                }
+            } 
+            
+
+            if(temp==nullptr){
+                // before breaking we have to make sure prevtail->next is connected to currhead !!
+                break;
+            }
+
+            ListNode* nexthead=temp->next;
+            temp->next=nullptr;
+            ListNode* newhead=reverse(currhead);
+
+            // immediately after reversing connect prev part !!
+            prevtail->next=newhead;
+            currhead->next=nexthead;
+            
+            prevtail=currhead;
+            temp=nexthead;
+
+        }
+        return dummy->next;
+    }
+};
