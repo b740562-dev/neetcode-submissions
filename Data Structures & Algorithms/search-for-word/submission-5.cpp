d00ff95bb54096019f47b8c767dd6bc32e4b409e
Bebo Modi <b740562@gmail.com>
2026-07-07 23:47:27 +0530

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    // k is ensuring whole word has been traversed or not !!!
+    bool solve(int r, int c, int m, int n, vector<vector<char>>& board, string word, int k) {
+        if (k == word.size()) {
+            return 1;
+        }
+
+        // board of i and j has been marked visited !!!
+        char ch=board[r][c];
+        board[r][c] = '.';
+        vector<int> row = {0, 1, 0, -1};
+        vector<int> col = {1, 0, -1, 0};
+
+        for (int p = 0; p < 4; p++) {
+            int nr = r + row[p];
+            int nc = c + col[p];
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n || board[nr][nc] == '.' || board[nr][nc] != word[k]) {
+                continue;
+            }
+            if (solve(nr, nc, m, n, board, word, k+1)) {
+                return true;
+            }
+        }
+        // need to rekeep the original character at it's original place because if this path marked false maybe some other place will mark it true !!!
+        // for that oiginal look of 2D arrat should remain same !!!!
+        board[r][c]=ch;
+        return false;
+    }
+
+   public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0] && solve(i, j, m, n, board, word, 1)==1) {
+                    return 1;
+                }
+            }
+        }
+        return false;
+    }
+};
