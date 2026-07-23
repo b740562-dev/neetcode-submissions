ee7df2dfc80afbd6fa10c1d44e788c6fddcdd011
Bebo Modi <b740562@gmail.com>
2026-07-23 15:05:58 +0530

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+    int f(int i,int j,vector<vector<int>>& dp,string& s, string& t){
+        // if target is empty string then it is always achievable !!!
+        if((i==0 && j==0) || j==0){
+            return 1;
+        }
+        if(i==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take=0;
+        if(s[i-1]==t[j-1]){
+            take=f(i-1,j-1,dp,s,t);
+        }
+        int nottake=f(i-1,j,dp,s,t);
+        return dp[i][j]=take+nottake;
+    }
+
+public:
+    int numDistinct(string s, string t) {
+
+        int n1=s.length(), n2=t.length();
+
+        if(n1<n2){
+            return 0;
+        }
+
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        return f(n1,n2,dp,s,t);
+    }
+};
