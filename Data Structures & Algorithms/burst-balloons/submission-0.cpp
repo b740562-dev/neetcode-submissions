3a2110923c863cb5eff373430e335a3aed456ece
Bebo Modi <b740562@gmail.com>
2026-07-23 21:59:21 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    int f(int i,int j,vector<int>& nums2,vector<vector<int>>& dp){
+        
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int maxi=0;
+        // choosing the ballon to be bursted at last is the most important trick here !!!!!
+        for(int ind=i;ind<=j;ind++){
+            maxi=max(maxi,nums2[i-1]*nums2[ind]*nums2[j+1]+f(i,ind-1,nums2,dp)+f(ind+1,j,nums2,dp));
+        }
+        return dp[i][j]=maxi;
+
+    }
+
+public:
+    int maxCoins(vector<int>& nums) {
+        
+        // we can choose any ballon to burst at last---represented by ind then it will have neighbours as i-1 and j+1 !!
+        int n=nums.size();
+        vector<int> nums2;
+        nums2.push_back(1);
+        for(auto &it:nums){
+            nums2.push_back(it);
+        }
+
+        nums2.push_back(1);
+
+        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+        return f(1,n,nums2,dp);
+
+    }
+};
